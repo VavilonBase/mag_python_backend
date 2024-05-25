165195e0827699f859ecf8a9b58026b16d71835f
Artem <VavilonYeah@yandex.ru>
2024-05-25 15:35:55 +0300

    s

--- router.py
+++ router.py
@@ -46,7 +46,7 @@ async def pay(data =Body()):
 async def pay_page(code: str = None):
    client_id="37B2979DA7A8F2BA802D236FF49625CBA9BB992A44F3DED85E193E32D86921C3" # TODO
    grant_type = "authorization_code"
-   redirect_uri = "http://194.59.40.99:8009/pay_page"
+   redirect_uri = "http://194.59.40.99:8009/pay_page1"
    headers= {
       "Content-Type": "application/x-www-form-urlencoded"
    }
@@ -67,6 +67,8 @@ async def pay_page(code: str = None):
       <body>
          <form method="POST" action="http://194.59.40.99:8009/pay">
             <input type="hidden" name="receiver" value="4100118691610961" />
+            <input name="project_number" />
+            <input name="author" />
             <input type="hidden" name="access_token" value={access_token} />
             <input name="sum" />
             <input type="submit" value="Перевести" />
@@ -82,7 +84,7 @@ async def pay_page(code: str = None):
 @router.get("/request_pay_page/", response_class=HTMLResponse)
 async def request_pay_page(code: str = None):
    client_id = "37B2979DA7A8F2BA802D236FF49625CBA9BB992A44F3DED85E193E32D86921C3"
-   redirect_uri = "http://194.59.40.99:8009/pay_page"
+   redirect_uri = "http://194.59.40.99:8009/pay_page1"
    headers= {
       "Content-Type": "application/x-www-form-urlencoded"
    }
