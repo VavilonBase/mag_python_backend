580055fd6a5c0850103252d1b18600c0d2765423
Artem <VavilonYeah@yandex.ru>
2024-05-25 16:21:14 +0300

    s

--- router.py
+++ router.py
@@ -14,47 +14,45 @@ router = APIRouter(
 )
 
 @router.post("/pay/")
-async def pay(data =Body()):
+async def pay(data = Body()):
    body = str(data).strip("'")
-   print(body)
+   
    receiver = parse_request(body, "receiver")
    access_token = parse_request(body, "access_token")
    summ = parse_request(body, "sum")
    project_number = parse_request(body, "project_number")
    author = parse_request(body, "author")
-   print(receiver)
-   print(access_token)
-   print(summ)
-   print(project_number)
-   print(author)
-   # Открывает конфиг
-   # f_config = open("./config.json")
-   # f_abi_account_contract = open("./contracts/AccountContract.json")
+   invest_number = parse_request(body, "invest_number")
+   
+   data = {
+      "pattern_id": "p2p",
+      "to": receiver,
+      "amount_due": summ,
+      "message": str(project_number) + str(author) + str(invest_number),
+      "label": str(project_number) + str(author) + str(invest_number)
+   }
+   
+   headers= {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Bearer " + str(access_token)
+   }
+   response = requests.post("https://yoomoney.ru/api/request-payment", 
+                            headers=headers,
+                            data=data)
+   response_data = response.json()
+   status = response_data["status"]
 
-   # config = json.load(f_config)
-   # abi_account_contract = json.load(f_abi_account_contract)
-
-   # owner_private_key = config["config"]["blockchain"]["owner_private_key"]
-   # rpc_server = config["config"]["blockchain"]["rpc_server"]
-   # account_contract_address = config["config"]["blockchain"]["smart_contracts"]["account_contract"]["address"]
-
-   # w3 = Web3(Web3.HTTPProvider(rpc_server))
-   # owner = Account.from_key(owner_private_key)
-
-   # account = Web3.to_checksum_address(account)
-   # account_contract = w3.eth.contract(address=account_contract_address, abi=abi_account_contract)
-   # print(account_contract.functions.getAccount(account).call({"from": owner.address}))
-   return {"status": "OK"}
+   return {"status": status}
 
 @router.get("/pay_page/", response_class=HTMLResponse)
-async def pay_page(project_number: int, author: str, code: str = None):
+async def pay_page(project_number: int, author: str, invest_number: int, code: str = None):
    headers= {
       "Content-Type": "application/x-www-form-urlencoded"
    }
    data={
       "code": code,
       "client_id": "37B2979DA7A8F2BA802D236FF49625CBA9BB992A44F3DED85E193E32D86921C3",
-      "redirect_uri": f"http://194.59.40.99:8009/pay_page?project_number={project_number}&author={author}",
+      "redirect_uri": f"http://194.59.40.99:8009/pay_page?project_number={project_number}&author={author}&invest_number={invest_number}",
       "grant_type": "authorization_code"
    }
    response = requests.post("https://yoomoney.ru/oauth/token", 
@@ -76,8 +74,9 @@ async def pay_page(project_number: int, author: str, code: str = None):
             <input type="hidden" name="receiver" value="4100118691610961" /><br/>
             <input type="hidden" value={project_number} name="project_number" /><br/>
             <input type="hidden" value={author} name="author" /><br/>
+            <input type="hidden" value={invest_number} name="invest_number" /><br/>
             <input type="hidden" name="access_token" value={access_token} /><br/>
-            <input name="sum" /><br/>
+            Введите сумму: <input name="sum" /> руб.<br/>
             <input type="submit" value="Перевести" />
          </form>
       </body>
@@ -89,13 +88,13 @@ async def pay_page(project_number: int, author: str, code: str = None):
 
 
 @router.get("/request_pay_page/", response_class=HTMLResponse)
-async def request_pay_page(project_number: int, author: str):
+async def request_pay_page(project_number: int, author: str, invest_number: int):
    headers= {
       "Content-Type": "application/x-www-form-urlencoded"
    }
    data={
       "client_id": "37B2979DA7A8F2BA802D236FF49625CBA9BB992A44F3DED85E193E32D86921C3",
-      "redirect_uri": f"http://194.59.40.99:8009/pay_page?project_number={project_number}&author={author}",
+      "redirect_uri": f"http://194.59.40.99:8009/pay_page?project_number={project_number}&author={author}&invest_number={invest_number}",
       "scope": "payment-p2p account-info operation-history"
    }
    response = requests.post("https://yoomoney.ru/oauth/authorize", 
@@ -111,8 +110,28 @@ async def u_money_notification(request: Request):
    
    amount = parse_request(body, "amount")
    label = parse_request(body, "label")
+   message = parse_request(body, "message")
    print(amount)
    print(label)
+   print(message)
+
+   #Открываем конфиг
+   f_config = open("./config.json")
+   f_abi_invest_contract = open("./contracts/InvestContract.json")
+
+   config = json.load(f_config)
+   abi_invest_contract = json.load(f_abi_invest_contract)
+
+   owner_private_key = config["config"]["blockchain"]["owner_private_key"]
+   rpc_server = config["config"]["blockchain"]["rpc_server"]
+   invest_contract_address = config["config"]["blockchain"]["smart_contracts"]["invest_contract"]["address"]
+
+   w3 = Web3(Web3.HTTPProvider(rpc_server))
+   owner = Account.from_key(owner_private_key)
+
+   account = Web3.to_checksum_address(account)
+   invest_contract = w3.eth.contract(address=invest_contract_address, abi=abi_invest_contract)
+   #print(invest_contract.functions.assignGetMoney(account).call({"from": owner.address}))
    return {"status": "OK"}
 
 
